505b5c28b44a35a7da1893265c2d213867155a7d
Carolyn Savas <carolyn.savas.keith@gmail.com>
2021-03-16 15:01:32 -0500

    Add files via upload

---
+++ HeapSortRunner.java
@@ -0,0 +1,20 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import static java.lang.System.*;
+
+public class HeapSortRunner
+{
+	public static void main ( String[] args )
+	{
+		HeapSort heap = new HeapSort();
+
+		int[] nums = {99,2,8,75,10,7,9,17,5,3,4,1,11,1};
+		
+		heap.heapSort(nums);
+		
+		System.out.println(heap);
+	}
+}
